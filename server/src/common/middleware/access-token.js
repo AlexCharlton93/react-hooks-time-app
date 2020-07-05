01d933da450a2c0fac8a55a079691461942c5c8b
alexcharlton93 <alex-charlton@live.co.uk>
2020-07-05 18:21:44 +0100

    Added requiresAccessToken middleware authentication to API

---
+++ server/src/common/middleware/access-token.js
@@ -0,0 +1,16 @@
+import { statusCodes } from '../constants';
+import { config } from '../config';
+
+export function requiresAccessToken(req, res, next) {
+	const header = req.headers['authorization'];
+
+	if (typeof header !== 'undefined') {
+        if (header !== config.accessToken) {
+            res.sendStatus(statusCodes.FORBIDDEN);
+        }
+
+		next();
+	} else {
+		res.sendStatus(statusCodes.FORBIDDEN);
+	}
+}
